231c903dd9ce2c9d0775fdd097fe0a788c1d4d9f
Christian Perez <perezchristian1012@gmail.com>
2019-12-16 20:05:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "cathy"
+    set :session_secret, "session_secret"
   end
 
   get '/' do
